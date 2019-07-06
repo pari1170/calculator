631e20964ff3eaf82b28aa0bd427799694e67c95
parish <shahoum@yahoo.com>
2019-07-06 20:04:14 +0430

    hi

---
+++ style.js
@@ -0,0 +1,26 @@
+﻿
+
+   
+    var c = document.getElementById("result");
+    //console.log(a);
+     
+    function sum() {
+        var a = Number(document.getElementById("first").value);
+        var b = Number(document.getElementById("second").value);
+        c.innerHTML += a + b + "</br>";
+    }
+    function minus() {
+        var a = Number(document.getElementById("first").value);
+        var b = Number(document.getElementById("second").value);
+        c.innerHTML += a - b +"</br>";
+    }
+    function multipl() {
+        var a = Number(document.getElementById("first").value);
+        var b = Number(document.getElementById("second").value);
+        c.innerHTML += a * b + "</br>";
+    }
+    function div() {
+        var a = Number(document.getElementById("first").value);
+        var b = Number(document.getElementById("second").value);
+        c.innerHTML += a / b + "</br>";
+    }
